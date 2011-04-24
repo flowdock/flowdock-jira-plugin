ae525658f46d4f7e9794ec29262c6d6eb9307992
Otto Hilska <otto.hilska@nodeta.fi>
2011-04-23 19:03:45 -0700

    Working configuration screen.

--- src/main/java/com/flowdock/plugins/jira/config/ConfigureFlowdockAction.java
+++ src/main/java/com/flowdock/plugins/jira/config/ConfigureFlowdockAction.java
@@ -1,5 +1,6 @@
 package com.flowdock.plugins.jira.config;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.atlassian.jira.web.action.JiraWebActionSupport;
@@ -9,6 +10,10 @@ public class ConfigureFlowdockAction extends JiraWebActionSupport {
 	
 	private FlowdockConfigurationManager flowdockConfigurationManager;
 	private List<ApiKeyPair> apiKeyPairs;
+	
+	// POST data
+	private String[] projectKeys;
+	private String[] apiKeys;
 	
 	public List<ApiKeyPair> getApiKeyPairs() {
 		return apiKeyPairs;
@@ -20,10 +25,32 @@ public class ConfigureFlowdockAction extends JiraWebActionSupport {
 
 	public ConfigureFlowdockAction() {
 	}
-
-	public String doDefault() {
+	
+	public String doExecute() {
 		this.updateTemplateData();
 		return INPUT;
+	}
+	
+	public String doSave() {
+		List<ApiKeyPair> pairs = this.parseApiKeyPairs();
+		this.flowdockConfigurationManager.setFlowdockApiKeys(pairs);
+		
+		return this.getRedirect("configureFlowdock.jspa");
+	}
+	
+	private List<ApiKeyPair> parseApiKeyPairs() {
+		List<ApiKeyPair> result = new ArrayList<ApiKeyPair>();
+		if (this.projectKeys == null || this.apiKeys == null || this.projectKeys.length != this.apiKeys.length) {
+			return result;
+		}
+		
+		for (int i=0; i<this.projectKeys.length; i++) {
+			if (this.apiKeys[i] != null && this.apiKeys[i] != "") {
+				result.add(new ApiKeyPair(this.projectKeys[i], this.apiKeys[i]));
+			}
+		}
+		
+		return result;
 	}
 	
 	/**
@@ -37,5 +64,13 @@ public class ConfigureFlowdockAction extends JiraWebActionSupport {
 	
 	private void updateTemplateData() {
 		this.apiKeyPairs = this.flowdockConfigurationManager.getFlowdockApiKeys();
+	}
+	
+	public void setProjectKeys(String[] projectKeys) {
+		this.projectKeys = projectKeys;
+	}
+	
+	public void setApiKeys(String[] apiKeys) {
+		this.apiKeys = apiKeys;
 	}
 }
